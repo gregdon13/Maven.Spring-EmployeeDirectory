32ac070060f3d28d9df78d4c537638fda5493cbc
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-16 13:18:50 -0400

    can update employee title and manager

--- src/main/java/io/zipcoder/controllers/EmployeeController.java
+++ src/main/java/io/zipcoder/controllers/EmployeeController.java
@@ -3,7 +3,9 @@ package io.zipcoder.controllers;
 import io.zipcoder.entities.Employee;
 import io.zipcoder.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,4 +22,24 @@ public class EmployeeController {
     public Employee createEmployee(Employee newHire) {
         return employeeRepository.save(newHire);
     }
+
+    @PutMapping("/company/employee/{id}")
+    public Employee updateEmployeeManager(@PathVariable Long id, Employee manager) {
+        Employee holder = employeeRepository.findOne(id);
+        holder.setManager(manager);
+        return employeeRepository.save(holder);
+    }
+
+    @PutMapping("/company/employee/{id}")
+    public Employee updateEmployeeTitle(@PathVariable Long id, String title) {
+        Employee holder = employeeRepository.findOne(id);
+        holder.setTitle(title);
+        return employeeRepository.save(holder);
+    }
+
+//    @PutMapping("/company/employee/{id}")
+//    public Employee updateEmployeeDepartmentNumber(@PathVariable Long id, Long departmentNumber) {
+//        Employee holder = employeeRepository.findOne(id);
+//        holder.setDepartmentNumber();
+//    }
 }
